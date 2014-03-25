bd4c02da2cfc2a597887e05e7bb00239dac6cf15
Christian Zangl <coralllama@gmail.com>
2014-03-25 21:36:56 +0100

    add sample

---
+++ sample/HjsonSample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hjson;
+
+namespace HjsonSample
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var data=(JsonObject)HjsonValue.Load("readme.hjson");
+      Console.WriteLine((string)data["hello"]);
+
+      Console.WriteLine("Saving as json...");
+      data.Save("readme.json", true);
+    }
+  }
+}
